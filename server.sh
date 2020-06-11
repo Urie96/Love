c9fffe6d575322fbdfa88cb0430163cb97770dc3
Yang Rui <urie@mail.ustc.edu.cn>
2020-06-11 18:10:41 +0800

    finish

--- server.sh
+++ server.sh
@@ -1,1 +1,8 @@
-docker build -t love .
+set -e
+docker build -t love:tmp .
+set +e
+docker stop love
+docker rmi -f love:latest
+docker tag love:tmp love:latest
+docker rmi love:tmp
+docker run -d -p 10000:80 --name love love:latest
